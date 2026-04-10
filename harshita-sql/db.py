62ef9a7ee8d25962fb1593a0033725261975faca
bhavika232 <bhavika06khanna@gmail.com>
2026-04-11 01:40:08 +0530

    keep only frontend

--- harshita-sql/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",  # Replace with YOUR password
-        database="printing_system"
-    )
