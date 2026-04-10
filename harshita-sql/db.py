f0553f52607cf60c45abfbe15744fe12d633e277
Harshita <harshita.kandanala@gmail.com>
2026-04-11 01:38:08 +0530

    remove hardcoded password from db.py

--- harshita-sql/db.py
+++ harshita-sql/db.py
@@ -1,9 +1,13 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",  # Replace with YOUR password
-        database="printing_system"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
